1eaad020cabc3fdb648e329894bfe20ad1498cf4
sanky1984@gmail.com <sanky1984@gmail.com>
2018-07-31 22:20:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeninningOfArray(array, element){
+  
+}
+var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic' ];
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+];
+
